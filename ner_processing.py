a5cc35175ce0d110c38ad6fbceb0614cfee516c6
flyangovoyang <45290558+flyangovoyang@users.noreply.github.com>
2020-08-21 16:47:35 +0800

    Create ner_processing.py

---
+++ ner_processing.py
@@ -0,0 +1,49 @@
+def conver_bio_to_bmes():
+    """ convert BIO schema to BMES tagging schema"""
+    input_file_path = 'test.txt'
+    output_file_path = 'test.data'
+    
+    with open(output_file_path, 'w', encoding='utf8') as fout:
+        sent = []
+        bmes = []
+        label = []
+        with open(input_file_path, 'r', encoding='utf8') as fin:
+            for line in fin:
+                line = line.strip()
+                if not line:
+                    # change bio -> bmes
+                    for index, token in enumerate(bmes):
+                        if token == 'B':
+                            # O B O x x -> O S O x x
+                            # B -> S
+                            # x x B -> x x S
+                            # B x x -> S x x
+                            if (0 < index < len(label) - 1 and bmes[index - 1] == 'O' and bmes[index + 1] == 'O') \
+                                    or (len(label) == 1) or (index == len(label) - 1) or (index == 0 and bmes[index + 1] == 'O'):
+                                bmes[index] = 'S'
+                        elif token == 'I':
+                            # x x I -> B E x x
+                            # x x I O/B -> B E O
+                            if index == len(label)-1 or (index < len(label)-1 and bmes[index+1] != 'I'):
+                                bmes[index] = 'E'
+                            # x x I I -> x x M I
+                            elif index < len(label)-1 and bmes[index+1] == 'I':
+                                bmes[index] = 'M'
+                    for s, b, l in zip(sent, bmes, label):
+                        if b != 'O':
+                            fout.write('{} {}-{}\n'.format(s, b, l))
+                        else:
+                            fout.write('{} {}\n'.format(s, b))
+                    fout.write('\n')
+                    sent = []
+                    bmes = []
+                    label = []
+                else:
+                    ll = line.split()
+                    sent.append(ll[0])
+                    if ll[1] == 'O':
+                        bmes.append('O')
+                        label.append('O')
+                    else:
+                        bmes.append(ll[1].split('-')[0])
+                        label.append(ll[1].split('-')[1])
