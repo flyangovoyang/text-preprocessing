d77fc7db1dcc01ce29e251470f2a20fd74fedf07
flyangovoyang <45290558+flyangovoyang@users.noreply.github.com>
2020-08-25 12:01:38 +0800

    Update ner_processing.py

--- ner_processing.py
+++ ner_processing.py
@@ -48,3 +48,21 @@ def conver_bio_to_bmes():
                     else:
                         bmes.append(ll[1].split('-')[0])
                         label.append(ll[1].split('-')[1])
+
+
+def convert_horizontal_to_vertical(input_file_path, output_file_path):
+    """ notice split('####')[x] should be modified according the acutal circumstance """
+    with open(output_file_path, 'w', encoding='utf8') as fout:
+        with open(input_file_path, 'r', encoding='utf8') as fin:
+            for line in fin:
+                line = line.strip()
+                if not line:
+                    continue
+                else:
+                    part = line.split('####')[2]
+                    pairs = part.split()
+                    for pair in pairs:
+                        ch = pair.split('=')[0]
+                        tag = pair.split('=')[1]
+                        fout.write('{} {}\n'.format(ch, tag))
+                    fout.write('\n')
