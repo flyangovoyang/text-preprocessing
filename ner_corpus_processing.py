0cde4dc1e8942e45c37ec1b16b74a47a7818bec1
flyangovoyang <45290558+flyangovoyang@users.noreply.github.com>
2020-10-21 17:08:17 +0800

    Update ner_corpus_processing.py

--- ner_corpus_processing.py
+++ ner_corpus_processing.py
@@ -1,8 +1,28 @@
 # ner corpus preprocessing scripts
+def convert_horizontal_to_vertical(input_file_path, output_file_path):
+    """ convert horizontal style to vertical style 
+    horizontal style: a=B-PER b=I-PER c=O\n
+    vertical   style: a=B-PER\nb=I-PER\bc=I-PER\n\n
+    """
+    with open(output_file_path, 'w', encoding='utf8') as fout:
+        with open(input_file_path, 'r', encoding='utf8') as fin:
+            for line in fin:
+                line = line.strip()
+                if not line:
+                    continue
+                else:
+                    part = line.split('####')[2]  # notice: 2 should be modified according to actual circumstance
+                    pairs = part.split()
+                    for pair in pairs:
+                        ch = pair.split('=')[0]
+                        tag = pair.split('=')[1]
+                        fout.write('{} {}\n'.format(ch, tag))
+                    fout.write('\n')
 
-def conver_bio_to_bmes(input_file_path, output_file_path):
+
+def convert_bio_to_bmes(input_file_path, output_file_path):
     """ convert BIO tagging schema to BMES tagging schema"""
-    
+
     with open(output_file_path, 'w', encoding='utf8') as fout:
         sent = []
         bmes = []
@@ -24,10 +44,10 @@ def conver_bio_to_bmes(input_file_path, output_file_path):
                         elif token == 'I':
                             # x x I -> B E x x
                             # x x I O/B -> B E O
-                            if index == len(label)-1 or (index < len(label)-1 and bmes[index+1] != 'I'):
+                            if index == len(label) - 1 or (index < len(label) - 1 and bmes[index + 1] != 'I'):
                                 bmes[index] = 'E'
                             # x x I I -> x x M I
-                            elif index < len(label)-1 and bmes[index+1] == 'I':
+                            elif index < len(label) - 1 and bmes[index + 1] == 'I':
                                 bmes[index] = 'M'
                     for s, b, l in zip(sent, bmes, label):
                         if b != 'O':
@@ -47,24 +67,3 @@ def conver_bio_to_bmes(input_file_path, output_file_path):
                     else:
                         bmes.append(ll[1].split('-')[0])
                         label.append(ll[1].split('-')[1])
-
-
-def convert_horizontal_to_vertical(input_file_path, output_file_path):
-    """ convert horizontal style to vertical style 
-    horizontal style: a=B-PER b=I-PER c=O\n
-    vertical   style: a=B-PER\nb=I-PER\bc=I-PER\n\n
-    """
-    with open(output_file_path, 'w', encoding='utf8') as fout:
-        with open(input_file_path, 'r', encoding='utf8') as fin:
-            for line in fin:
-                line = line.strip()
-                if not line:
-                    continue
-                else:
-                    part = line.split('####')[2]  # notice: 2 should be modified according to actual circumstance
-                    pairs = part.split()
-                    for pair in pairs:
-                        ch = pair.split('=')[0]
-                        tag = pair.split('=')[1]
-                        fout.write('{} {}\n'.format(ch, tag))
-                    fout.write('\n')
